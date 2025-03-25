f4045ce6216053c3b4789c498b8dd813c2fb92a2
Vitaly Tsurikov <tsurikow.v@gmail.com>
2025-03-25 12:57:01 +0300

    Added execute permissions to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Ensure .env file exists
 if [ ! -f ".env" ]; then
     echo ".env file not found. Using .env.dist as default."
     cp .env.dist .env
 fi
 
-# Stop running containers (if any)
 docker-compose down
 
-# Build and start the project
 docker-compose up --build -d
 
 echo "Deployment completed successfully!"
